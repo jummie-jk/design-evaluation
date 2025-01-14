c3de724c229721580c8f450988c33751367f6f63
jummie-jk <jummiejk21@gmail.com>
2025-01-14 10:04:09 +0100

    created component for settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DevicePageComponent } from './pages/device-page/device-page.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DevicePageComponent
+    DevicePageComponent,
+    SettingsPageComponent
   ],
   imports: [
     BrowserModule,
